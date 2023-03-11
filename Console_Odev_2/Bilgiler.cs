faf2baef3bbfbf2949f429c7143ba2ad98943a4e
m.sinans <msomercikoglu@gmail.com>
2023-03-11 14:30:36 +0300

    Add project files.

---
+++ Console_Odev_2/Bilgiler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Odev_2
+{
+    internal class Bilgiler
+    {
+        public void bilgi(string name, string job, string age)
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(job);
+            Console.WriteLine(age);
+        }
+    }
+}
---
+++ Console_Odev_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Odev_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bilgiler bg = new Bilgiler();
+            string adsoyad, meslek, yas;
+            Console.WriteLine("İsim Giriniz!: ");
+            adsoyad = Console.ReadLine();
+            Console.WriteLine("Meslek Giriniz!: ");
+            meslek = Console.ReadLine();
+            Console.WriteLine("Yaş Giriniz!: ");
+            yas = Console.ReadLine();
+            bg.bilgi("Ad/Soyad: " + adsoyad, "Meslek: " + meslek, "Yaş: " + yas);
+            Console.Read();
+
+        }
+    }
+}
